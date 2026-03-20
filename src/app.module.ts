57f8a66c9e009b88c45012c19c30de92098e265f
Anunay Argha <adobex092@gmail.com>
2026-03-20 13:22:58 +0600

    project init with proper structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { FilesModule } from './files/files.module';
+import { NotificationsGateway } from './notifications/notifications.gateway';
+import { NotificationsService } from './notifications/notifications.service';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProjectsModule,
+    TasksModule,
+    FilesModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, NotificationsGateway, NotificationsService],
 })
 export class AppModule {}
